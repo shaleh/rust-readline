266a87153a550720724b909e3029f0598ad664fd
Ian Quick <ian.quick@gmail.com>
2015-04-12 10:44:23 -0400

    updated for rust 1.0.0 beta

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #![crate_type = "lib"]
 extern crate libc;
 
-use std::c_str;
+use std::ffi::CString;
+use std::ffi::CStr;
+use std::string::String;
 
 mod ext_readline {
     use libc::c_char;
+
     #[link(name = "readline")]
     extern {
         pub fn add_history(line: *const c_char);
@@ -14,19 +17,23 @@ mod ext_readline {
 
 pub fn add_history(line: &str) {
     unsafe {
-        ext_readline::add_history(line.to_c_str().as_ptr());
+        ext_readline::add_history(CString::new(line).unwrap().as_ptr());
     }
 }
 
 pub fn readline(prompt: &str) -> Option<String> {
-    let cprmt = prompt.to_c_str();
+    let cprmt = CString::new(prompt).unwrap().as_ptr();
     unsafe {
-        let ret = ext_readline::readline(cprmt.as_ptr());
+        let ret = ext_readline::readline(cprmt);
         if ret.is_null() {  // user pressed Ctrl-D
             None
         }
         else {
-            c_str::CString::new(ret, true).as_str().map(|ret| ret.to_string())
+            let slice = CStr::from_ptr(ret);
+
+            let bytes = slice.to_bytes();
+
+            Some(String::from_utf8_lossy(bytes).into_owned())
         }
     }
 }
