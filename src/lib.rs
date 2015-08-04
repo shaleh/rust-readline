284755316f3f1b6f34007cfa9a41674cce743fa6
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2015-08-04 12:52:50 -0700

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! A Simple wrapper for libreadline or libedit
+//!
+//! Exports two functions:
+//!
+//!   - `add_history`
+//!   - `readline`
+//!
+
 extern crate libc;
 
 use std::ffi::CString;
@@ -13,12 +21,20 @@ mod ext_readline {
     }
 }
 
+/// Update the internal history of input lines
+///
+/// Call this after a successful `readline()` call to add that line to the
+/// history.
 pub fn add_history(line: &str) {
     unsafe {
         ext_readline::add_history(CString::new(line).unwrap().as_ptr());
     }
 }
 
+/// Invoke the external `readline()`. 
+///
+/// Returns an `Option<String>` representing whether a `String` was returned
+/// or NULL. `None` indicates the user has signal end of input.
 pub fn readline(prompt: &str) -> Option<String> {
     let cprmt = CString::new(prompt).unwrap().as_ptr();
     unsafe {
